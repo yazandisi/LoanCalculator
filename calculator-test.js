6aea946d3dcb9be5401a7b49aacad52002021f51
Yazan Eldisi <yazandisi@gmail.com>
2022-03-23 12:56:31 -0500

    Adding describe

--- calculator-test.js
+++ calculator-test.js
@@ -1,26 +1,28 @@
-it("should calculate the monthly rate correctly", function () {
-  const values = {
-    amount: 5000,
-    years: 5,
-    rate: 2.7,
-  };
-  expect(calculateMonthlyPayment(values)).toEqual("89.18");
-});
+describe("Test cal monthly payments", function () {
+  it("should calculate the monthly rate correctly", function () {
+    const values = {
+      amount: 5000,
+      years: 5,
+      rate: 2.7,
+    };
+    expect(calculateMonthlyPayment(values)).toEqual("89.18");
+  });
 
-it("should return a result with 2 decimal places", function () {
-  const values = {
-    amount: 5432,
-    years: 5.5,
-    rate: 2.7,
-  };
-  expect(calculateMonthlyPayment(values)).toEqual("88.66");
-});
+  it("should return a result with 2 decimal places", function () {
+    const values = {
+      amount: 5432,
+      years: 5.5,
+      rate: 2.7,
+    };
+    expect(calculateMonthlyPayment(values)).toEqual("88.66");
+  });
 
-it("Should tell user to fill in years if 0", function () {
-  const values = {
-    amount: 1000,
-    years: 0,
-    rate: 2.7,
-  };
-  expect(calculateMonthlyPayment(values)).toEqual("Please fill in Years");
+  it("Should tell user to fill in years if 0", function () {
+    const values = {
+      amount: 1000,
+      years: 0,
+      rate: 2.7,
+    };
+    expect(calculateMonthlyPayment(values)).toEqual("Please fill in Years");
+  });
 });
